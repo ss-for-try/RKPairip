f949b4fe1dc8ce19770d0819f516f480e04b9e76
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2025-08-04 00:39:23 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-import setuptools
+from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="Pairip",
-    version="3.6",
-    author="RK_TECHNO_INDIA",
-    author_email="TechnoIndia786@gmail.com",
-    description="Recover String & Rebuild Apk",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license='MIT',
-    url="https://github.com/Technoindian/RKPairip",
-    keywords='RKPairip',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.12',
-    entry_points={
-        'console_scripts': [
-            'RKPairip=RKPairip.__main__:RK_Techno_IND',
-        ],
-    },
-)
+setup(packages=find_packages())
